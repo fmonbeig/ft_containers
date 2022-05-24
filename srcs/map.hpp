232c3cf9f7b9708659c80def8a43674aa2c78ef8
Florian MONBEIG <fmonbeig@e3r1p10.42.fr>
2022-05-24 16:33:32 +0200

    commencement de map

--- srcs/map.hpp
+++ srcs/map.hpp
@@ -6,7 +6,7 @@
 /*   By: fmonbeig <fmonbeig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 14:32:52 by fmonbeig          #+#    #+#             */
-/*   Updated: 2022/05/24 14:55:48 by fmonbeig         ###   ########.fr       */
+/*   Updated: 2022/05/24 15:26:24 by fmonbeig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,3 +54,23 @@ class map
 
 }
 #endif
+
+// Map utilise des arbres pour equilibré l'acces a ses données
+// A verifier
+// Map ne peut avoir que des cles unique
+// Les elements vont etre choisi par une fonction (autre classe ?) Compare
+// Dans un arbre binaire on range les elements comme suit : si nouveau mot et au dessus du dernier alors on le met a droite sinon c'est a gauche
+// Pas opti car si toujours d'un cote alors l'arbre est desequilibré
+
+/*  https://fr.wikipedia.org/wiki/Arbre_bicolore
+les règles suivantes sont utilisées :
+
+1 - Un nœud est soit rouge soit noir ;
+2 - La racine est noire ;
+3 - Les enfants d'un nœud rouge sont noirs;
+4 - Tous les nœuds ont 2 enfants. Ce sont d'autres nœuds ou des feuilles NIL, qui ne possèdent pas de valeur et qui sont les seuls nœuds sans enfants. Leur couleur est toujours noire et rentre donc en compte lors du calcul de la hauteur noire.
+5 - Le chemin de la racine à n'importe quelle feuille (NIL) contient le même nombre de nœuds noirs. On peut appeler ce nombre de nœuds noirs la hauteur noire.
+
+	https://fr.wikipedia.org/wiki/Arbre_binaire_de_recherche
+	https://fr.wikipedia.org/wiki/Arbre_AVL
+*/
--- srcs/reverse_iterator.hpp
+++ srcs/reverse_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: fmonbeig <fmonbeig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 10:57:09 by fmonbeig          #+#    #+#             */
-/*   Updated: 2022/05/20 15:29:48 by fmonbeig         ###   ########.fr       */
+/*   Updated: 2022/05/24 14:58:50 by fmonbeig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -154,54 +154,6 @@ namespace ft
 	operator-( typename reverse_iterator<Iter>::difference_type n, const reverse_iterator<Iter>& it )
 	{ return reverse_iterator<Iter>(it.base() + n); }
 }
-
-
-	// template <class it>
-	// class reverse_iterator {
-	// public:
-	// 	// -structors
-	// 	reverse_iterator			(void)												{ _it = it(); }
-	// 	reverse_iterator			(typename it::value_type * ptr)						{ _it = it(ptr); }
-	// 	reverse_iterator			(const it & x)										{ _it = x; --_it; }
-	// 	~reverse_iterator			(void)												{}
-	// 	// Conversion
-	// 	template <class U>			friend class										reverse_iterator;
-	// 	template <class U>
-	// 	reverse_iterator			(const reverse_iterator<U> & x)						{ _it = x.getIt(); }
-
-	// 	// Assignment
-	// 	reverse_iterator &			operator=	(const reverse_iterator & x)			{ _it = x.getIt(); return (*this); }
-	// 	reverse_iterator &			operator+=	(int n)									{ _it -= n; return (*this); }
-	// 	reverse_iterator &			operator-=	(int n)									{ _it += n; return (*this); }
-	// 	// Comparison
-	// 	template <class U> bool		operator==	(const reverse_iterator<U> & x) const	{ return (_it == x.getIt()); }
-	// 	template <class U> bool		operator!=	(const reverse_iterator<U> & x) const	{ return (_it != x.getIt()); }
-	// 	template <class U> bool		operator<	(const reverse_iterator<U> & x) const	{ return (_it > x.getIt()); }
-	// 	template <class U> bool		operator>	(const reverse_iterator<U> & x) const	{ return (_it < x.getIt()); }
-	// 	template <class U> bool		operator<=	(const reverse_iterator<U> & x) const	{ return (_it >= x.getIt()); }
-	// 	template <class U> bool		operator>=	(const reverse_iterator<U> & x) const	{ return (_it <= x.getIt()); }
-	// 	// -crementation
-	// 	reverse_iterator &			operator++	(void)									{ --_it; return (*this); }
-	// 	reverse_iterator &			operator--	(void)									{ ++_it; return (*this); }
-	// 	reverse_iterator			operator++	(int)									{ reverse_iterator<it> x(*this); --_it; return (x); }
-	// 	reverse_iterator			operator--	(int)									{ reverse_iterator<it> x(*this); ++_it; return (x); }
-	// 	// Operation
-	// 	reverse_iterator			operator+	(int n) const							{ return (_it - n + 1); }
-	// 	reverse_iterator			operator-	(int n) const							{ return (_it + n + 1); }
-	// 	std::ptrdiff_t				operator-	(const reverse_iterator & x) const		{ return (x.getIt() - _it); }
-	// 	// Dereference
-	// 	typename it::value_type &	operator[]	(size_t n) const						{ return (*(_it - n)); }
-	// 	typename it::value_type &	operator*	(void) const							{ return (*_it); }
-	// 	typename it::value_type *	operator->	(void) const							{ return (&(*_it)); }
-	// 	// Member functions
-	// 	it							base		(void)									{ return (++it(_it)); }
-	// 	it							getIt		(void) const							{ return (_it); }
-	// 	// Non-member functions
-	// 	friend reverse_iterator		operator+	(int n, const reverse_iterator & x)		{ return (x.getIt() - n + 1); }
-
-	// private:
-	// 	it		_it;
-	// };
 
 
 #endif
